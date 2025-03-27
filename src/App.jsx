a16b739235facd9379b5d251a06500a878e61e9c
OlenaB <elena.yefimchuk@gmail.com>
2025-03-26 22:55:38 -0600

    fix move to App without step 6

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,43 @@
 import Description from './components/Description/Description';
 import Options from './components/Options/Options';
 import Feedback from './components/Feedback/Feedback';
+import Notification from './components/Notification/Notification';
 import { useState } from 'react';
 
 const App = () => {
+  const [counts, setcounts] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+  const handleClick = option => {
+    setcounts({
+      ...counts,
+      [option]: counts[option] + 1,
+    });
+  };
+  const totalFeedback = counts.good + counts.neutral + counts.bad;
+  const handleClickReset = () => {
+    setcounts({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+  };
+
   return (
     <>
       <Description />
-      <Options />
+      <Options
+        handleClick={handleClick}
+        totalFeedback={totalFeedback}
+        handleClickReset={handleClickReset}
+      />
+      {totalFeedback === 0 ? (
+        <Notification />
+      ) : (
+        <Feedback feedback={counts} totalFeedback={totalFeedback} />
+      )}
     </>
   );
 };
--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -2,21 +2,15 @@ import React from 'react';
 import Notification from '../Notification/Notification';
 
 const Feedback = ({ feedback, totalFeedback }) => {
-  // const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
-  {
-    if (totalFeedback === 0) {
-      return <Notification />;
-    }
-    return (
-      <>
-        <p>Good: {feedback.good} </p>
-        <p>Neutral: {feedback.neutral}</p>
-        <p>Bad: {feedback.bad}</p>
-        <p>Total: {totalFeedback}</p>
-        <p>Positive: {Math.round((feedback.good / totalFeedback) * 100)} %</p>
-      </>
-    );
-  }
+  return (
+    <>
+      <p>Good: {feedback.good} </p>
+      <p>Neutral: {feedback.neutral}</p>
+      <p>Bad: {feedback.bad}</p>
+      <p>Total: {totalFeedback}</p>
+      <p>Positive: {Math.round((feedback.good / totalFeedback) * 100)} %</p>
+    </>
+  );
 };
 
 export default Feedback;
